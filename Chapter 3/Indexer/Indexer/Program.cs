eeb9cec03adbeb594f9814edcf388c7c9cd16e2d
AbdallahEld <abdallaheldesoky301@gmail.com>
2026-04-05 16:17:36 +0200

    Indexer

---
+++ Chapter 3/Indexer/Indexer/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Indexer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Team team = new();
+            team[0] = "Alice";
+            team[1] = "Bob";
+            team[2] = "Charlie";
+
+            Console.WriteLine(team[0]); // Output: Alice
+        }
+    }
+}
---
+++ Chapter 3/Indexer/Indexer/Team.cs
@@ -0,0 +1,20 @@
+﻿namespace Indexer
+{
+    public class Team
+    {
+        private List<string> players = new();
+
+        public string this[int i]
+        {
+            get => players[i];
+            set
+            {
+                while (players.Count <= i)
+                {
+                    players.Add(string.Empty);
+                }
+                players[i] = value;
+            }
+        }
+    }
+}
